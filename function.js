3e86a080d98c7407fb2c4ec28d4aef5479f2c797
Goldzik2 <98888507+Goldzik2@users.noreply.github.com>
2022-02-24 13:38:18 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+id; 'functions.js',
+exports; {'C:\Users\Asus\Desktop\SteelRP\commands'}
+parent; null,
+filename; '/test.js',
+loaded; false,
+children; ['handlers'],
+paths;
+[
+    'C:\Users\Asus\Desktop\SteelRP\commands',
+    'C:\Users\Desktop\SteelRP\commands',
+    '/Users/commands',
+    '/commands'
+]
+module.exports.getUser = (commands) => {
+  exports.commands.getUser (functions.js)
+}
+require(commands)
+require(functions.js)
+module.exports.getUsers = (functions.js); {
+  { getUser: [exports] [getUser], getUsers; [exports] [getUsers] }
+}
